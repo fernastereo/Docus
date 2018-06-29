488955f18c81011b5f1ac024314e2046a6587a10
fernast <fernandoecueto@gmail.com>
2018-06-28 19:20:45 -0500

    Deploy de version 1.0

--- database/migrations/2018_06_28_212110_alter_table_documents_modify_comments_field.php
+++ database/migrations/2018_06_28_212110_alter_table_documents_modify_comments_field.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterTableDocumentsModifyCommentsField extends Migration
+class AlterTableDocumentsModifyCommentsField2 extends Migration
 {
     /**
      * Run the migrations.
