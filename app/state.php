4a22c947ac4d6342564ad9549f7364a473bf2a30
Fernando Cueto <fernandoecueto@gmail.com>
2021-03-27 17:55:09 -0500

    bug con namespace Typedocument

--- app/state.php
+++ app/state.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class State extends Model
 {
     public function documents(){
-    	return $this->hasMany(Document::class);
+        return $this->hasMany(Document::class);
     }
 }
