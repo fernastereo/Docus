aa0291f520fe1237d5fe858f30ada5d89e1a466f
Fernando Cueto <fernandoecueto@gmail.com>
2021-03-27 16:53:38 -0500

    bug con namespace Typedocument

--- app/typedocument.php
+++ app/typedocument.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Typedocument extends Model
 {
     public function documents(){
-    	return $this->hasMany(Document::class);
+    	return $this->hasMany('App\Document');
     }
 }
